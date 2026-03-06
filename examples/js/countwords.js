e9464f185015e26549d6394be76b72871581a838
Jon Craton <jncraton@gmail.com>
2026-03-06 09:19:47 -0500

    Use more familiar equality

--- examples/js/countwords.js
+++ examples/js/countwords.js
@@ -3,7 +3,7 @@ function countWords(text) {
   let inWhitespace = true
 
   for (let char of text) {
-    if (char === ' ' || char === '\n') {
+    if (char == ' ' || char == '\n') {
       inWhitespace = true
     } else if (inWhitespace) {
       count += 1
@@ -14,6 +14,6 @@ function countWords(text) {
   return count
 }
 
-console.assert(countWords('Hello world') === 2)
-console.assert(countWords('   extra   spaces   ') === 2)
-console.assert(countWords('') === 0)
+console.assert(countWords('Hello world') == 2)
+console.assert(countWords('   extra   spaces   ') == 2)
+console.assert(countWords('') == 0)
