391be85cd87fd6bd29e8671e7db81d9ba9b30eaf
Jon Craton <jncraton@gmail.com>
2026-03-06 09:47:30 -0500

    Use Python spacing for js

--- examples/countwords/countwords.js
+++ examples/countwords/countwords.js
@@ -1,17 +1,17 @@
 function count_words(text) {
-  let count = 0
-  let in_whitespace = true
+    let count = 0
+    let in_whitespace = true
 
-  for (let char of text) {
-    if (char == " " || char == "\n") {
-      in_whitespace = true
-    } else if (in_whitespace) {
-      count += 1
-      in_whitespace = false
+    for (let char of text) {
+        if (char == " " || char == "\n") {
+            in_whitespace = true
+        } else if (in_whitespace) {
+            count += 1
+            in_whitespace = false
+        }
     }
-  }
 
-  return count
+    return count
 }
 
 console.assert(count_words("Hello world") == 2)
