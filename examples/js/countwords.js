a0693fa28267b53c273238555935443e5140fb28
Jon Craton <jncraton@gmail.com>
2026-03-06 09:19:27 -0500

    Add countwords.js

---
+++ examples/js/countwords.js
@@ -0,0 +1,19 @@
+function countWords(text) {
+  let count = 0
+  let inWhitespace = true
+
+  for (let char of text) {
+    if (char === ' ' || char === '\n') {
+      inWhitespace = true
+    } else if (inWhitespace) {
+      count += 1
+      inWhitespace = false
+    }
+  }
+
+  return count
+}
+
+console.assert(countWords('Hello world') === 2)
+console.assert(countWords('   extra   spaces   ') === 2)
+console.assert(countWords('') === 0)
