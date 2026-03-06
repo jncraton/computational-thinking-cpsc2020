82599a60421440a13e52d270fe92a98bef5899de
Jon Craton <jncraton@gmail.com>
2026-03-06 09:21:30 -0500

    Use Python-style naming conventions

--- examples/js/countwords.js
+++ examples/js/countwords.js
@@ -1,19 +1,19 @@
-function countWords(text) {
+function count_word(text) {
   let count = 0
-  let inWhitespace = true
+  let in_whitespace = true
 
   for (let char of text) {
     if (char == " " || char == "\n") {
-      inWhitespace = true
-    } else if (inWhitespace) {
+      in_whitespace = true
+    } else if (in_whitespace) {
       count += 1
-      inWhitespace = false
+      in_whitespace = false
     }
   }
 
   return count
 }
 
-console.assert(countWords("Hello world") == 2)
-console.assert(countWords("   extra   spaces   ") == 2)
-console.assert(countWords("") == 0)
+console.assert(count_word("Hello world") == 2)
+console.assert(count_word("   extra   spaces   ") == 2)
+console.assert(count_word("") == 0)
